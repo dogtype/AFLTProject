debbda70ed278594aabfc2f575ce09201a32dcc1
hofmannj <hofmannj@student.ethz.ch>
2022-06-06 15:49:45 +0200

    implementing some basic functionality

--- fgglib/src/fgg.py
+++ fgglib/src/fgg.py
@@ -4,21 +4,27 @@ class FGG:
     # • T is a set of terminal symbols
     # • N is a set of nonterminal symbols
     # • S is a string (or label) for the starting nonterminal
-    # • P is a dict, mapping nonterminal (left) to sequences of terminals and non-terminals (right)
+    # • P is a set of productions, which are in turn tuples of head and body
 
     def __init__(self, T, N, S, P):
         self.T = T
         self.N = N
         self.S = S
-        self.P = P
+        self._P = P
 
-    def P() -> Generator:
+    @property
+    def P(self) -> Generator:
         """ returns a generator for all production rules """
-        raise NotImplementedError
+		for p in self._P.items():
+			yield p
 
-    def size() -> number:
+    @property
+    def size(self) -> number:
         """ returns the size of the grammar """
-        raise NotImplementedError
+        size = 0
+		for (_, body) in self.P:
+			size+=len(body)+1
+		return size
 
     def recursive(self) -> bool:
         """ checks if the grammar contains recursive production rules """
@@ -32,23 +38,38 @@ class FGG:
         """ checks if the grammar is reentrant """
         raise NotImplementedError
 
-    def conjunction(fgg : FGG) -> FGG:
+    def conjunction(self,fgg : FGG) -> FGG:
         """ implements the conjunction algorithm for factor graph grammars """
         raise NotImplementedError
 
-    def add(head, body) -> bool:
+    def add(self, head, body) -> bool:
         """ helper function to add production rules """
-        self.P[head] = body
+        if not isinstance(head, NT):
+			raise InvalidProduction
+		self.N.add(head)
 
-    def copy() -> FGG:
+		for elem in body:
+			if isinstance(elem, NT):
+				self.N.add(elem)
+			elif isinstance(elem, Sym):
+				self.T.add(elem)
+			else:
+				raise InvalidProduction
+
+		self._P.add(Production(head, body))
+        
+    def copy(self) -> FGG:
         """ returns a deepcopy of the entire grammar """
-        raise NotImplementedError
+		return copy.deepcopy(self)
 
-    def print() -> string:
+    def print(self) -> string:
         """ returns the grammar in a printable string format """
-        raise NotImplementedError
+        string = "start: " + self.S + "\n"
+        for p in self.P:
+            string += p.head + "--->" + p.body +"\n"
+        return string
 
-    def cyclic() -> bool:
+    def cyclic(self) -> bool:
         """ returns if the grammar has cyclic productions (probably not needed) """
         raise NotImplementedError
 
@@ -60,14 +81,14 @@ class FGGsum_product:
     def __init__(self, fgg):
         self.fgg = fgg
 
-    def inference()-> number:
+    def inference(self)-> number:
         """ returns the sum product of a factor graph grammar for the general case """
         raise NotImplementedError
 
-    def inference_finite_variables() -> number:
+    def inference_finite_variables(self) -> number:
         """ returns the sum product of a factor graph grammar with finite variable domain """
         raise NotImplementedError
 
-    def inference_finite_states() -> number:
+    def inference_finite_states(self) -> number:
         """ returns the sum product of a factor graph grammar with finite number of states """
         raise NotImplementedError
