886dbb0f8a8e13948264ac76b0c3a8a08a910bbe
hofmannj <hofmannj@student.ethz.ch>
2022-06-07 15:18:15 +0200

    created simple testing file

---
+++ fgglib/autotesting/fggtest.py
@@ -0,0 +1,23 @@
+def recursive_test_example():
+    raise NotImplementedError
+
+def linearly_recursive_test_example():
+    raise NotImplementedError
+
+def reentrant_test_examle():
+    raise NotImplementedError
+
+def size_test_example():
+    raise NotImplementedError
+
+def cyclic_test_example():
+    raise NotImplementedError
+
+def conjunction_test_example():
+    raise NotImplementedError
+
+def inference_finite_variables_test_example():
+    raise NotImplementedError
+
+def inference_finite_states_test_example():
+    raise NotImplementedError
