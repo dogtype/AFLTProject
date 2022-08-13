4acd6168c2ebf96237e8571c85621db91c998ec5
jolee1101 <1101jolee@gmail.com>
2022-08-13 11:53:17 +0200

    added imgname variable in visualization; updated presentation. missing part: inference

--- fgglib/presentation/visualization.py
+++ fgglib/presentation/visualization.py
@@ -2,36 +2,36 @@ from fgglib.fgg.fgg import FGG
 
 class Visualization:
               
-    def draw(self, graph):         
+    def draw(self, graph, name=None):         
         import pygraphviz as pgv         
-        """ returns a png image of the graph """         
-        G, filename = self.visualize(graph)         
+        """ returns a png image of the graph """                 
+        G, filename = self.visualize(graph,name)
         G.layout(prog="neato") # one of: neato|dot|twopi|circo|fdp|nop         
-        filename+=".png"         
-        return G.draw(filename)      
+        filename="./fgglib/presentation/images/"+str(filename)+".png"   
+        return G.draw(filename)
     
-    def visualize(self,graph):
+    def visualize(self,graph, name):
         import pygraphviz as pgv    
         G = pgv.AGraph(strict = False, directed = False)   
-        filename = ""           
-        if isinstance(graph,FGG):           
-            filename = "FGG"        
+        name = str(name)
+        if isinstance(graph,FGG):                       
+            if str(name) == "None": name = "FGG"
             for p in  graph.P:      
                 body = p.body        
                 for e in body.E:       
                     G.add_node(e.label,shape="circle")       
-                    for v in body.V:                
-                        G.add_node(v.label,shape="box")       
+                for v in body.V:                
+                    G.add_node(v.label,shape="box")    
+                for v in e.targets:    
+                    G.add_edge(e.label, v.label, color = "black")        
         else:        
-            filename = "Graph"     
+            if str(name) == "None": name = "Grammar"
             for v in graph.V:           
                 G.add_node(v.label,shape="circle")     
-                for e in graph.E:        
-                    G.add_node(e.label,shape="box", color ="red")    
-                    for e in graph.E:         
-                        for v in e.targets:    
-                            G.add_edge(e.label, v.label, color = "black")
-        
-        G.graph_attr["label"] = filename  
+            for e in graph.E:        
+                G.add_node(e.label,shape="box", color ="red")    
+                for tgt in e.targets:    
+                    G.add_edge(e.label, tgt.label, color = "black")                                            
+        G.graph_attr["label"] = name  
         G.node_attr["color"] = "black"     
-        return G, filename
+        return G, name
